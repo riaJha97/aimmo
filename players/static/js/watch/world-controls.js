68c335ef4480ce245a977186acf5e81349d318e3
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-08-03 17:00:09 +0100

    The world initialises now.

--- players/static/js/watch/world-controls.js
+++ players/static/js/watch/world-controls.js
@@ -52,6 +52,10 @@ const CONTROLS = Object.create({
         var healthPoints = mapFeatures["health_point"]
         var pickups = mapFeatures["pickup"]
 
+        if (this.world.layout === null) {
+            this.world.layout = {};
+        }
+
         // Create obstacles.
         for (var obstacleToCreate in obstacles["create"])
         {
@@ -101,7 +105,8 @@ function worldInit(data) {
     CONTROLS.initialiseWorld(data.width, data.height, data.layout, data.minX, data.minY, data.maxX, data.maxY);
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
+
     var world = {};
     world.players = {};
     VIEWER.init(document.getElementById("watch-world-canvas"), world, APPEARANCE);
@@ -109,9 +114,10 @@ $(document).ready(function(){
 
     if (ACTIVE) {
         var socket = io.connect(GAME_URL_BASE, { path: GAME_URL_PATH });
-        socket.on('world-init'), function() {
+        socket.on('world-init', function() {
+            console.log("World initialised.");
             worldInit();
-        }
+        });
 
         socket.on('world-update', function(msg) {
             worldUpdate(msg);
